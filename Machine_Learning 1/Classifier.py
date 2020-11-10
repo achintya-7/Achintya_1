2e5fda42b9c492de033283882c81018deccc1779
achintya-7 <67036708+achintya-7@users.noreply.github.com>
2020-11-10 11:00:49 +0530

    Add files via upload

---
+++ Machine_Learning 1/Classifier.py
@@ -0,0 +1,33 @@
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+# Descripton of data
+#        - sepal length in cm
+#        - sepal width in cm
+#        - petal length in cm
+#        - petal width in cm
+#        - class:
+#          - Iris-Setosa
+#          - Iris-Versicolour
+#          - Iris-Virginica
+
+# LOADING DATASET
+iris = datasets.load_iris()
+# print(iris.DESCR)
+
+# ASSIGNING AND PRINTING FEATURES AND LABELS
+features = iris.data
+labels = iris.target
+# print(features[0], labels[0])
+
+
+# TRAINING THE CLASSIFIERS
+clf = KNeighborsClassifier()
+
+# all classifiers generally have a fitting command, clf.fit is used to the features and labels
+clf.fit(features, labels)
+
+# here you can write the numerical value about the flower in the predict function. The values should correspond to the
+# description as mentioned above in the code.
+predo = clf.predict([[1, 2, 5, 0]])
+print(predo)
